d8fc8d9225cea0a0ca2d31ce806b56e25d9b06b7
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-31 12:13:31 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,15 @@
+// inputBtn.addEventListener("click",function(){
+//     console.log("btn clicked")
+// })
+
+// inputBtn.addEventListener("click",print)
+// function print(){
+//     console.log("printed")
+// }
+// const name="pragala"
+// console.log(name)
+
+// const array=["apple","orange"]
+// console.log(array)
+// array="new"
+// console.log(array)   //Assignment to constant variable.
---
+++ index.js
@@ -0,0 +1,21 @@
+const myLeads=["apple","orange"]
+const inputBtn=document.getElementById("input-btn")
+const inputEl=document.getElementById("input-el")
+const ulEl=document.getElementById("ul-el")
+const containerEl=document.getElementById("container")
+
+inputBtn.addEventListener("click",function(){
+   myLeads.push(inputEl.value)
+//    console.log(myLeads)
+  
+})
+
+for(let i=0;i<myLeads.length;i++){
+
+    // console.log(myLeads[i])
+    ulEl.innerHTML+= "<li>"+ myLeads[i]+ "</li> "
+    
+}
+containerEl.innerHTML+="<button> click me </button>"
+
+
