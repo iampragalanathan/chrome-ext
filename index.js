b8171a1e4fb4ec0c2096ae8053f2f46f7b2b4e89
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-31 14:41:22 +0530

    add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ for(let i=0;i<myLeads.length;i++){
 
     // console.log(myLeads[i])
     // ulEl.innerHTML+= "<li>"+ myLeads[i]+ "</li> "
-    listItems+="<li>"  +myLeads[i] +  "</li>"
+    listItems+="<li> <a href="+myLeads[i] + "</a></li>"
     
 }
 ulEl.innerHTML=listItems
