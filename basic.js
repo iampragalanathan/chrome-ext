39c089e37592ae8e187f604fd9969e32ff1b5e62
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-01 14:50:06 +0530

    add

--- basic.js
+++ basic.js
@@ -13,3 +13,58 @@
 // console.log(array)
 // array="new"
 // console.log(array)   //Assignment to constant variable.
+
+
+// ==============innerHTML=======================
+
+// containerEl.innerHTML+="<button onclick='btnclick()'> click me </button>"
+
+// const cont=document.getElementById("container")
+// cont.innerHTML="<button id='butn'> click me </button>"
+
+// document.getElementById("butn").addEventListener("click",function(){
+//     cont.innerHTML+= "<img style='display:block'id='immg'src='https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg'>"
+// })
+
+// ==============Localstorage===================
+
+// localStorage.setItem("myName",[123,"mango",12.5,"kiwi"])
+// localStorage.setItem("user" ,123)
+// localStorage.setItem("product","iphone")
+// const local=localStorage.getItem("myName")
+// localStorage.removeItem("user")
+// localStorage.clear()
+// console.log(local)
+// console.log(typeof local)
+
+// let mobile=["iphone","realme","infinix","redmi"]
+// // console.log(mobile)
+// localStorage.setItem("brand",JSON.stringify(mobile))
+// let smartphone =JSON.parse(localStorage.getItem("brand"))
+// mobile.push("nothing","tecno")
+// console.log(mobile)
+// localStorage.setItem("brand",JSON.stringify(mobile))
+//  smartphone =JSON.parse(localStorage.getItem("brand"))
+
+// let fruits=`["apple","orange"]`
+// fruits=JSON.parse(fruits)
+// fruits.push("kiwi")
+// localStorage.setItem("fruitsArr",JSON.stringify(fruits))
+// const fromLocal=JSON.parse(localStorage.getItem("fruitsArr"))
+// console.log(fromLocal)
+
+// =========Truthy or Falsy====================
+
+// console.log(Boolean(undefined))   //falsy
+// console.log(Boolean("")) //falsy
+// console.log(Boolean(null)) //falsy
+// console.log(Boolean(0)) //falsy
+// console.log(Boolean(false)) //falsy
+// console.log(Boolean("false")) //truthy
+// console.log(Boolean(true)) //truthy
+// console.log(Boolean("hi")) //truthy
+// console.log(Boolean([])) //truthy
+// console.log(Boolean({})) //truthy
+// console.log(Boolean(1235)) //truthy
+// console.log(Boolean(NaN)) //falsy
+
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputBtn=document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
 const containerEl=document.getElementById("container")
+const deleteEl=document.getElementById("delete-btn")
 
 
 let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myleads"))
@@ -16,6 +17,22 @@ else{
     
     ulEl.textContent="no data to show,please give input and click save input button"
 }
+
+
+deleteEl.addEventListener('click',function(){
+
+     localStorage.clear()
+     myLeads=[]
+    //  console.log(myLeads)
+    renderLeads()
+
+  if(leadsFromLocalStorage ===null && myLeads.length <=0){
+
+    ulEl.textContent="no data to show"
+  }
+
+     
+})
 
 
 
@@ -44,59 +61,6 @@ for(let i=0;i<myLeads.length;i++){
 }
 ulEl.innerHTML=listItems
 }
-
-// ==============innerHTML=======================
-
-// containerEl.innerHTML+="<button onclick='btnclick()'> click me </button>"
-
-// const cont=document.getElementById("container")
-// cont.innerHTML="<button id='butn'> click me </button>"
-
-// document.getElementById("butn").addEventListener("click",function(){
-//     cont.innerHTML+= "<img style='display:block'id='immg'src='https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg'>"
-// })
-
-// ==============Localstorage===================
-
-// localStorage.setItem("myName",[123,"mango",12.5,"kiwi"])
-// localStorage.setItem("user" ,123)
-// localStorage.setItem("product","iphone")
-// const local=localStorage.getItem("myName")
-// localStorage.removeItem("user")
-// localStorage.clear()
-// console.log(local)
-// console.log(typeof local)
-
-// let mobile=["iphone","realme","infinix","redmi"]
-// // console.log(mobile)
-// localStorage.setItem("brand",JSON.stringify(mobile))
-// let smartphone =JSON.parse(localStorage.getItem("brand"))
-// mobile.push("nothing","tecno")
-// console.log(mobile)
-// localStorage.setItem("brand",JSON.stringify(mobile))
-//  smartphone =JSON.parse(localStorage.getItem("brand"))
-
-// let fruits=`["apple","orange"]`
-// fruits=JSON.parse(fruits)
-// fruits.push("kiwi")
-// localStorage.setItem("fruitsArr",JSON.stringify(fruits))
-// const fromLocal=JSON.parse(localStorage.getItem("fruitsArr"))
-// console.log(fromLocal)
-
-// =========Truthy or Falsy====================
-
-// console.log(Boolean(undefined))   //falsy
-// console.log(Boolean("")) //falsy
-// console.log(Boolean(null)) //falsy
-// console.log(Boolean(0)) //falsy
-// console.log(Boolean(false)) //falsy
-// console.log(Boolean("false")) //truthy
-// console.log(Boolean(true)) //truthy
-// console.log(Boolean("hi")) //truthy
-// console.log(Boolean([])) //truthy
-// console.log(Boolean({})) //truthy
-// console.log(Boolean(1235)) //truthy
-// console.log(Boolean(NaN)) //falsy
 
 
 
